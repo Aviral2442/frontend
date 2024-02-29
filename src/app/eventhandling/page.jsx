7030cb80544e10ecc28759d691f3d986e18a965f
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-02-29 18:32:07 +0530

    react

---
+++ src/app/eventhandling/page.jsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+
+const EventHandling = () => {
+  return (
+    <div className='container' >
+        
+        <h1 className='text-center' >Event Handling</h1>
+        <hr />
+        <button className='btn btn-primary' onClick={ () => { alert('Hey ! Aviral') } } >Click Me</button>
+        <input type="color" onChange={ (e) => { document.body.style.background = e.target.value } } />
+    </div> 
+  )
+}
+
+export default EventHandling;
