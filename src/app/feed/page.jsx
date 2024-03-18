efd361e4593fdb2060dd792bdefcba60171507a4
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-03-18 18:47:06 +0530

    sucessfully display on frontend , all post are visible

--- src/app/feed/page.jsx
+++ src/app/feed/page.jsx
@@ -1,8 +1,15 @@
 'use client'
-import React, { useEffect } from 'react'
+import { IconHeartBolt, IconShare, IconWriting } from '@tabler/icons-react';
+import React, { useEffect, useState } from 'react'
 
 
 const Feed = () => {
+
+    // useState
+
+    const [postArray, setPostArray] = useState([]);
+
+    //data comming from databased & we are displaying on frontend 
 
     const fetchPostData = () => {
 
@@ -15,6 +22,7 @@ const Feed = () => {
 
         .then( (data) =>  {
             console.log(data);
+            setPostArray(data);
         }  )
 
         .catch((err) => {
@@ -35,6 +43,31 @@ const Feed = () => {
 
         <div className="container p-5">
             <div className="text-center">Trending Posts</div>
+
+
+        <div className="col-md-6 mx-auto">
+            {
+                postArray.map( (post) => { 
+                    return <div key={post._id} className='card shadow mb-5' >
+                        <div className="class-header">
+                            <h4 className='p-3 text-center' >{post.title}</h4>
+                        </div>
+                        <img className='card-img-top' src={post.image} alt="" />
+                        <div className="card-footer">
+                            <div className="d-flex g4">
+                                <button className="btn-outline-primary w-100"><IconHeartBolt></IconHeartBolt> {post.likes}  </button>
+                                <button className="btn-outline-primary w-100"><IconShare></IconShare> {post.shares}  </button>
+                                <button className="btn-outline-primary w-100"><IconWriting></IconWriting> {post.comment}  </button>
+
+                            </div>
+                        </div>
+
+                    </div>
+                 } )
+            }
+        </div>
+
+
         </div>
 
     </div>
